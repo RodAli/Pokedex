279e59d06ebea6b1a3a821889cc2d2100f3a2394
Rod Mazloomi <rod.mazloomi@hotmail.com>
2023-12-28 12:20:09 -0500

    Added preliminary pokemon base stat chart

--- src/PokemonDetailPage.tsx
+++ src/PokemonDetailPage.tsx
@@ -3,6 +3,7 @@ import { getPokemon } from "./NetworkController";
 import { PokemonDetail, PokemonType } from "./Types";
 import "./styles/PokemonDetailPage.css"
 import { TypeBadge } from "./TypeBadge";
+import { Chart } from "react-google-charts";
 
 type LoaderResponse = {
     pokemonData: PokemonDetail;
@@ -15,7 +16,12 @@ export async function PokemonDetailPageLoader({ params }: any): Promise<LoaderRe
 
 const PokemonDetailPage = () => {
     const { pokemonData } = useLoaderData() as LoaderResponse;
-    console.log(pokemonData);
+
+    const statData: any[][] = [["Name", "Value"]];
+    pokemonData.stats.forEach(stat => {
+        statData.push([stat.stat.name, stat.base_stat])
+    });
+
     return <div id="detail-page-container">
         <img id="pokemon-sprite" src={pokemonData.sprites.other["official-artwork"].front_default} width={100} height={100}/>
         
@@ -26,13 +32,36 @@ const PokemonDetailPage = () => {
 
         <div id="type-container">
             <TypeBadge type={pokemonData.types[0]?.type.name} />
-            <div style={{width: "7px"}}></div>
-            {pokemonData.types[1] ? <TypeBadge type={pokemonData.types[1]?.type.name} /> : null}
+            {pokemonData.types[1] ? <>
+                <div style={{width: "7px"}}></div>
+                <TypeBadge type={pokemonData.types[1]?.type.name} />
+            </> : null}
         </div>
         
         
-        {pokemonData.stats.map((s, i) => <div key={i}>{s.stat.name} {s.base_stat}</div>)}
+        {/* {pokemonData.stats.map((s, i) => <div key={i}>{s.stat.name} {s.base_stat}</div>)} */}
+        <Chart 
+            chartType="BarChart"
+            width="100%"
+            height="400px"
+            data={statData}
+            options={options}
+        />
+        
+
     </div>;
 }
+
+  export const options = {
+    title: "Hello",
+    chartArea: { width: "50%" },
+    hAxis: {
+      title: "Value",
+      minValue: 0,
+    },
+    vAxis: {
+      title: "Name",
+    },
+  };
 
 export default PokemonDetailPage;
